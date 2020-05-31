decc1d899bf7de22ec8c8fb4ae7ae130067d2351
tsuny221 <kingoftsuny@gmail.com>
2020-05-31 14:37:53 +0000

    [Finish] 応用課題2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_action :baria_user, only: [:update]
+      before_action :authenticate_user!
+	before_action :baria_user, only: [:edit, :update]
 
   def show
   	@user = User.find(params[:id])
@@ -10,7 +11,7 @@ class UsersController < ApplicationController
   def index
   	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
   	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
-
+  end
   def edit
   	@user = User.find(params[:id])
   end
@@ -18,9 +19,9 @@ class UsersController < ApplicationController
   def update
   	@user = User.find(params[:id])
   	if @user.update(user_params)
-  		redirect_to users_path(@user), notice: "successfully updated user!"
+  		redirect_to user_path(@user), notice: "successfully updated user!"
   	else
-  		render "show"
+  		render :edit
   	end
   end
 
@@ -31,7 +32,8 @@ class UsersController < ApplicationController
 
   #url直接防止　メソッドを自己定義してbefore_actionで発動。
    def baria_user
-  	unless params[:id].to_i == current_user.id
+    @user = User.find(params[:id])
+  	if @user != current_user
   		redirect_to user_path(current_user)
   	end
    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
   devise_for :users
-  root 'home#top'
-  get 'home/about'
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books
+  root to: 'home#top'
+  get 'home/about' => 'home#about'
+end
